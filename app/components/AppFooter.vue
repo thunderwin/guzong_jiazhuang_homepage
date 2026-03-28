66231bad56ae89d04d7d50fc748cc85f2dac2280
thunderwin <woooms@qq.com>
2026-03-28 11:00:12 +0800

    Deploy

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -9,9 +9,10 @@
 
   <UFooter>
     <template #left>
-      <p class="text-sm text-muted">
-        绎间科技 • 全屋设计出海一体化平台 • © {{ new Date().getFullYear() }}
-      </p>
+      <div class="text-sm text-muted leading-6">
+        <p>绎间科技 • 全屋设计出海一体化平台 • © {{ new Date().getFullYear() }}</p>
+        <p>地址：广东省佛山市南海区桂城街道<br>德高信写字楼3座802室</p>
+      </div>
     </template>
 
     <template #right />
--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="inline-flex items-center gap-2 text-primary font-semibold whitespace-nowrap">
-    <UIcon
-      name="i-lucide-sofa"
-      class="size-5"
-    />
-    <span class="text-sm sm:text-base leading-none">绎间科技</span>
-  </div>
+  <img
+    src="/logo.svg"
+    alt="绎间科技"
+    class="h-8 w-auto sm:h-9"
+  >
 </template>
